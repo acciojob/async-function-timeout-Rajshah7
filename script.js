f68b39dee02dca6fb9ba959acca350fb4b431ef8
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2026-05-28 11:12:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+let btn = document.getElementById('btn');
+let text = document.getElementById('text');
+let delay = document.getElementById('delay');
+let output = document.getElementById('output');
+
+function printDelay(text,delay){
+	return new Promise(() => {
+		setTimeout(() => {
+		output.innerHTML = `
+			<div>
+				${text} - ${delay}
+			</div>
+		`
+	},delay * 1000);
+	})
+}
+
+async function callFunction(text,delay){
+	try{
+		await printDelay(text,delay);
+	}catch(e){
+		console.log("err");
+	}
+}
+
+btn.addEventListener('click', () => {
+	callFunction(text.value,delay.value);
+})
+
